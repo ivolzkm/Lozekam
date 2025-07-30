0cab1102c83894fe4808c7dc63dbf1f91e3e242e
Ivo Ricardo Lozekam Junior <141505581+ivolzkm@users.noreply.github.com>
2025-07-30 17:32:55 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@
 function Home() {
   return (
     <div className="container">
-      <h1 className="main-message">Duda te amo</h1>
-      <p className="sub-message">Esta é a minha forma de dizer que estou aprendendo a programar para te impressionar. ❤️</p>
+      <h1 className="main-message">Ivo Ricardo Lozekam</h1>
+      <p className="sub-message">Esta é a minha forma de dizer que estou aprendendo a programar para te impressionar, pai. ❤️</p>
     </div>
   );
 }
